996a41663365a728349b965444980337d4b78ba1
victoire1806 <victoire1806@yahoo.com>
2021-10-03 10:00:51 -0400

    Update

--- index.js
+++ index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const readme = "README.md";
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: "input",
+        message: "Project Title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Project Description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation instructions for this project? ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Provide instructions on how to use your project:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Contributors for this project? ",
+        name: "contribution"
+    },
+  
+    {
+        type: "input",
+        message: "What are the testing instructions for this project? ",
+        name: "testing"
+    },
+    {
+        type: "checkbox",
+        message: "Please select a license for this project:",
+        choices: [
+            "Apache_License_2.0",
+            "Boost_Software_License 1.0",
+            "MIT",
+            "ISC",
+            "GNU_LGPLv3",
+            "Mozilla_Public_License_2.0",
+            "Eclipse_Punlic_License",
+        ],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username? ",
+        name: "gitName"
+    },
+    {
+        type: "input",
+        message: "What is your email address? ",
+        name: "email"
+    }
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+        return console.log(err);
+      }
+      else{
+        return console.log("Your README.md file has been generated")
+      }
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to initialize app
+function init() {
+    inquirer.prompt(questions).then(userdata => writeToFile(readme, userdata));
+}
+ 
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,56 @@
+//let badge = ![License badge](https://img.shields.io/badge/License-${data.license}-green);
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {
+
+//if(license.data = lincense.data) ()
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {
+
+//let link =  
+//}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Description
+  ${data.description}
+  ## Licence Badge
+  ![License badge](https://img.shields.io/badge/License-${data.license}-green)
+  
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Tests](#testing)
+  5. [Contribution](#contribution)
+  6. [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${data.license}
+  [License badge](https://img.shields.io/badge/License-${data.license}-green)
+ 
+  ## Tests
+  ${data.testing}
+  ## Contribution
+  ${data.contribution}
+  
+  ## Questions
+  * If you have questions, email me at: ${data.email},
+  * GitHub Profile link: https://github.com/${data.gitName}
+  
 
 `;
 }
